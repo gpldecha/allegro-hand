0daff54ff811ee9345a4caad718705a060a74e53
Guillaume de Chambrier <g.dechambrier@ocado.com>
2018-07-23 13:37:54 +0000

    removed build

--- ahand_hw/build/CMakeFiles/ahand_hw.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ahand_hw.
-# This may be replaced when dependencies are built.
--- ahand_hw/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guillaume/catkin_ws/src/allegro-hand/ahand_hw
-# Build directory: /home/guillaume/catkin_ws/src/allegro-hand/ahand_hw/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
