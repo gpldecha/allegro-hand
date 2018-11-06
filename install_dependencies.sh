9c45a46f0d62c2e09542d98669181ccc748ea288
Guillaume de Chambrier <g.dechambrier@ocado.com>
2018-11-06 17:09:41 -0100

    added joint-limit

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -57,6 +57,7 @@ install_pcan_basic
 install_library ros-${ROS_DISTRO}-control-toolbox
 install_library ros-${ROS_DISTRO}-controller-manager
 install_library ros-${ROS_DISTRO}-transmission-interface 
+install_library ros-${ROS_DISTRO}--joint-limits-interface
 
 install_library ros-${ROS_DISTRO}-gazebo9-ros-pkgs
 install_library ros-${ROS_DISTRO}-gazebo9-dev
