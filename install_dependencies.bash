66181e063dc886b34a63b5c4573c952d4e62f515
Guillaume de Chambrier <g.dechambrier@ocado.com>
2018-11-06 13:29:32 -0100

    updated dependencies

--- install_dependencies.bash
+++ install_dependencies.bash
@@ -7,21 +7,6 @@ NC='\033[0m'
 ROS_DISTRO=$(ls /opt/ros/) 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-install_peak_linux_driver(){
-	printf "${GREEN}installing peak linux driver\n${NC}"
-	cd ${DIR}/peak-linux-driver/
-	make NET=NO
-	make install
-	modprobe pcan
-}
-
-install_pcan_basic(){
-	printf "${GREEN}installing pcan bais\n${NC}"
-	cd ${DIR}/pcan-basic/pcanbasic/
-	make 
-	make install
-	cd ../../
-}
 
 install_library(){
 	LIBRARY=$(dpkg -S $1 )
@@ -34,9 +19,72 @@ install_library(){
 	fi
 }
 
-install_library linux-headers-$(uname -r)
+install_peak_linux_driver(){
+	if [[ ! -f /usr/include/libpcan.h ]]; then
+		printf "${GREEN}installing peak linux driver\n${NC}"
+		cd ${DIR}/peak-linux-driver/
+		make NET=NO
+		make install
+		modprobe pcan
+		cd ${DIR}
+	else
+		printf "${GREEN}peak linux driver ... ok\n${NC}"
+	fi
+}
+
+install_pcan_basic(){
+	if [[ !  /usr/include/PCANBasic.h ]]; then
+		printf "${GREEN}installing pcan basic api\n${NC}"
+		cd ${DIR}/pcan-basic/pcanbasic/
+		make 
+		make install
+		cd ${DIR}
+	else
+		printf "${GREEN}pcan basic api ... ok\n${NC}"
+	fi
+}
+
+install_gazebo(){
+	read -p "Do you want to install gazebo9, this will purge all other installations  (y/n) "  -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		printf "${GREEN}removing gazebo\n${NC}"
+		apt-get -q --yes --force-yes remove '.*gazebo.*' '.*sdformat.*' '.*ignition-math.*' '.*ignition-msgs.*' '.*ignition-transport.*' > /dev/null
+		apt-get -q --yes --force-yes autoremove  > /dev/null
+		sudo find /usr/local -name '*gazebo*' | while read line; do
+			echo "removing file '$line'" 
+			sudo rm -fr $line
+		done
+		sudo find /usr/include -name '*gazebo*' | while read line; do
+			echo "removing file '$line'" 
+			sudo rm -fr $line
+		done
+		sudo find /usr/lib -name '*gazebo*' | while read line; do
+			echo "removing file '$line'" 
+			sudo rm -fr $line
+		done
+		apt-get -q --yes --force-yes update  > /dev/null
+		printf "${GREEN}adding osrfoundation to sources gazebo\n${NC}"
+		sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+		wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+		apt-get --yes update > /dev/null
+		install_library ros-${ROS_DISTRO}-gazebo9-ros-pkgs
+		install_library ros-${ROS_DISTRO}-gazebo9-ros-control
+	else
+		echo "ok"
+	fi	
+}
+
 install_library libncurses5-dev 
-install_library libncursesw5-dev
+install_library libncurses5
 install_library libpopt-dev
+
+install_library linux-headers-$(uname -r)
 install_peak_linux_driver
 install_pcan_basic
+
+install_gazebo
+install_library ros-${ROS_DISTRO}-dartsim
+install_library ros-${ROS_DISTRO}-forward-command-controller
+
+
